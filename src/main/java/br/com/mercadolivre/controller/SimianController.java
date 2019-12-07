fd6295d680ffd0bda2e1efa699b25a1d7161bad1
Vinícius Duarte <vinidual@gmail.com>
2019-12-07 12:07:20 -0300

    Added postgres rds and liquibase config

--- src/main/java/br/com/mercadolivre/controller/SimianController.java
+++ src/main/java/br/com/mercadolivre/controller/SimianController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Arrays;
 
 @RestController("simian")
 @Validated
--- src/main/java/br/com/mercadolivre/repository/SimianEntity.java
+++ src/main/java/br/com/mercadolivre/repository/SimianEntity.java
@@ -4,12 +4,8 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.UUID;
 
